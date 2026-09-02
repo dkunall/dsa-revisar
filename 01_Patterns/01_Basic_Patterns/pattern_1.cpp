5086f4df78fd2b4c74ec0e9cca4686586dd10c1f
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-02 19:48:51 +0530

    Correct pattern printing function in pattern_1.cpp
    Fix syntax errors and improve pattern printing logic.

--- 01_Patterns/01_Basic_Patterns/pattern_1.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_1.cpp
@@ -5,6 +5,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print(int n)
+{
+    for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+                {
+                    cout<<"*";
+                }
+        }
+    return;
+}
+
+
 int main() {
+    int n;
+    cin>>n;
+    print(n)
     return 0;
 }
