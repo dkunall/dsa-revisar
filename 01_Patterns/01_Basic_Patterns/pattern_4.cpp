2629d789d337f755e40348bbe04df298afa14716
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-06 10:07:26 +0530

    Implement pattern printing function
    Added a function to print a pattern based on user input.

--- 01_Patterns/01_Basic_Patterns/pattern_4.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_4.cpp
@@ -2,9 +2,32 @@
 // Approach: brute -> better -> optimal
 // Time:  Space:
 
-#include <bits/stdc++.h>
+1
+22
+333
+4444
+55555
+1
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n)
+{
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            {
+                cout<<i;
+            }
+        cout<<endl;
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
