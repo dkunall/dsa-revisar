ef4e0c0e3a9b8cfad5b70700fa683286ab2188fb
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-06 10:06:06 +0530

    Update pattern_3.cpp

--- 01_Patterns/01_Basic_Patterns/pattern_3.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_3.cpp
@@ -2,9 +2,32 @@
 // Approach: brute -> better -> optimal
 // Time:  Space:
 
-#include <bits/stdc++.h>
+1
+12
+123
+1234
+12345
+
+    
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n)
+{
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            {
+                cout<<j;
+            }
+        cout<<endl;
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
