7d3b64dd9d1cc9c5a74ed3a604f6c4d697c1b2dc
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 11:15:48 +0530

    Update pattern_7.cpp

--- 01_Patterns/01_Basic_Patterns/pattern_7.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_7.cpp
@@ -2,9 +2,40 @@
 // Approach: brute -> better -> optimal
 // Time:  Space:
 
-#include <bits/stdc++.h>
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*/
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n)
+{
+    for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=n-1;j++)
+                {
+                    cout<<" ";
+                }
+
+            for(int j=1;j<=2*i-1;j++)
+                {
+                    cout<<"*";
+                }
+            cout<<endl;
+        }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
