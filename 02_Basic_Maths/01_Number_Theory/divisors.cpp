936497dcd87218456b1ac85304b443d01a118114
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 15:28:11 +0530

    Update divisors.cpp

--- 02_Basic_Maths/01_Number_Theory/divisors.cpp
+++ 02_Basic_Maths/01_Number_Theory/divisors.cpp
@@ -1,10 +1,15 @@
-// Problem:
-// Approach: brute -> better -> optimal
-// Time:  Space:
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+
     return 0;
 }
