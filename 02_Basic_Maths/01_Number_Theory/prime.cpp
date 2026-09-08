ce3a50ed4d2bb77fc3f9492d70b4be5c37a780bf
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 17:46:15 +0530

    Update prime.cpp

--- 02_Basic_Maths/01_Number_Theory/prime.cpp
+++ 02_Basic_Maths/01_Number_Theory/prime.cpp
@@ -1,10 +1,51 @@
-// Problem:
-// Approach: brute -> better -> optimal
-// Time:  Space:
+// 1. Brute Force
+bool isPrime(int n) {
+    if (n < 2)
+        return false;
 
-#include <bits/stdc++.h>
-using namespace std;
+    int count = 0;
 
-int main() {
-    return 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0)
+            count++;
+    }
+
+    return count == 2;
+}
+// 2. Optimized - O(n)
+bool isPrime(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+// 3. Optimized - O(sqrt(n)) ⭐
+bool isPrime(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+// 4. Recursive
+bool isPrime(int n, int i = 2) {
+    if (n < 2)
+        return false;
+
+    if (i * i > n)
+        return true;
+
+    if (n % i == 0)
+        return false;
+
+    return isPrime(n, i + 1);
 }
