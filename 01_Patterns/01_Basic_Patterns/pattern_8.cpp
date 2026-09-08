9af36c8c8dfb055b957c25220f241e3b642d81f0
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 11:26:14 +0530

    Update pattern_8.cpp

--- 01_Patterns/01_Basic_Patterns/pattern_8.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_8.cpp
@@ -2,9 +2,34 @@
 // Approach: brute -> better -> optimal
 // Time:  Space:
 
-#include <bits/stdc++.h>
+// Problem:
+// Approach: brute -> better -> optimal
+// Time:  Space:
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n)
+{
+    for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+                {
+                    cout<<" ";
+                }
+
+            for(int j=1;j<=2*i-1;j++)
+                {
+                    cout<<"*";
+                }
+            cout<<endl;
+        }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
