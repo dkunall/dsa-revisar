5c28a9aa5618b4e33a985291c4d68004b96d9bb6
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 15:07:12 +0530

    Update pattern_11.cpp

--- 01_Patterns/01_Basic_Patterns/pattern_11.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_11.cpp
@@ -2,9 +2,33 @@
 // Approach: brute -> better -> optimal
 // Time:  Space:
 
-#include <bits/stdc++.h>
+
+/*
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+*/
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n) {
+    for (int i = 1; i <= n; i++) {
+        int start = (i % 2 == 0) ? 0 : 1;
+
+        for (int j = 1; j <= i; j++) {
+            cout << start << " ";
+            start = 1 - start;
+        }
+
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
