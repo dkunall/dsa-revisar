85abf51596f1fcea366b92140f4e4f6009c7e2f2
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 15:27:43 +0530

    Update count_digits.cpp

--- 02_Basic_Maths/01_Number_Theory/count_digits.cpp
+++ 02_Basic_Maths/01_Number_Theory/count_digits.cpp
@@ -1,10 +1,18 @@
-// Problem:
-// Approach: brute -> better -> optimal
-// Time:  Space:
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    while (n > 0) {
+        count++;
+        n /= 10;
+    }
+
+    cout << count;
+
     return 0;
 }
