dfa7273abc91c8a21ed474d6b4df4011e097788b
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 15:14:46 +0530

    Update pattern_12.cpp

--- 01_Patterns/01_Basic_Patterns/pattern_12.cpp
+++ 01_Patterns/01_Basic_Patterns/pattern_12.cpp
@@ -1,10 +1,31 @@
 // Problem:
 // Approach: brute -> better -> optimal
 // Time:  Space:
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(int n) {
+    for (int i = 1; i <= n; i++) {
+
+        // Left numbers
+        for (int j = 1; j <= i; j++)
+            cout << j;
+
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++)
+            cout << " ";
+
+        // Right numbers
+        for (int j = i; j >= 1; j--)
+            cout << j;
+
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
