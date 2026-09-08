8cdb6f75341e137e05d6b231b07df0ef99982fca
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-08 15:40:45 +0530

    Update gcd.cpp

--- 02_Basic_Maths/01_Number_Theory/gcd.cpp
+++ 02_Basic_Maths/01_Number_Theory/gcd.cpp
@@ -1,10 +1,34 @@
-// Problem:
-// Approach: brute -> better -> optimal
-// Time:  Space:
+// 1. Brute Force
+int gcd(int a, int b) {
+    for (int i = min(a, b); i >= 1; i--) {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
+    return 1;
+}
+// 2. Subtraction - Iterative
+int gcd(int a, int b) {
+    while (a != b) {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
+}
+// 3. Euclidean Algorithm - Iterative
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+// 4. Euclidean Algorithm - Recursive
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
 
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    return 0;
+    return gcd(b, a % b);
 }
