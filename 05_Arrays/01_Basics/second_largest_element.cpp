a7041f8d094af030a09afb3d4d18cb77cf4ca374
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-13 10:08:24 +0530

    Update second_largest_element.cpp

--- 05_Arrays/01_Basics/second_largest_element.cpp
+++ 05_Arrays/01_Basics/second_largest_element.cpp
@@ -5,6 +5,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int second_largest(vector<int> arr)
+{
+    int lar=INT_MIN;
+    int slar=INT_MIN;
+
+    for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>lar)
+            {
+                slar=lar;
+                lar=arr[i];
+            }
+
+            if(arr[i]<lar && arr[i]>slar)
+            {
+                slar=arr[i];
+            }
+        }
+    return slar;
+}
+
 int main() {
+    int p=second_largest({3,6,8,9,6,5,4,89,56,3,576,5});
+    cout<<p;
     return 0;
 }
