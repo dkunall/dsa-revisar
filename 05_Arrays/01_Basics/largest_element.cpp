bb2f51dea97c2aa9e044395abeb5367a5f3bafa3
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-13 10:12:59 +0530

    Correct largest element function implementation
    Fix the largest function to correctly find the largest element in the array.

--- 05_Arrays/01_Basics/largest_element.cpp
+++ 05_Arrays/01_Basics/largest_element.cpp
@@ -5,6 +5,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int largest(vector<int> arr)
+{
+    int lar=INT_MIN;
+
+    for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>lar)
+            {
+                lar=arr[i];
+            }
+        }
+    return lar;
+}
+
 int main() {
+    int p=largest({3,6,8,9,6,5,4,89,56,3,576,5});
+    cout<<p;
     return 0;
 }
