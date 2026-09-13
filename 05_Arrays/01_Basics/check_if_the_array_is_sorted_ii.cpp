2079b6c010c08bb932161501167089774c78cb2a
Kunal Kumar <98274739+dkunall@users.noreply.github.com>
2026-09-13 10:42:26 +0530

    Add function to check if array is sorted
    Implemented a function to check if an array is sorted in non-decreasing order.

--- 05_Arrays/01_Basics/check_if_the_array_is_sorted_ii.cpp
+++ 05_Arrays/01_Basics/check_if_the_array_is_sorted_ii.cpp
@@ -1,10 +1,26 @@
-// Problem:
-// Approach: brute -> better -> optimal
-// Time:  Space:
+// Problem: Check if an array is sorted
+// Approach: Brute -> Better -> Optimal
+// Time: O(n)
+// Space: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+bool is_sorted(vector<int> arr)
+{
+    for(int i = 0; i < arr.size() - 1; i++)
+    {
+        if(arr[i] > arr[i + 1])
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    bool p = is_sorted({1, 2, 3, 4, 67, 90});
+    cout << p;
+
     return 0;
 }
